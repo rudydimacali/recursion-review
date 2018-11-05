bb062101a1c2e2e8acc331ab9177e477f5acaaa6
Hack Reactor Student <student@hackreactor.com>
2018-11-05 11:53:04 -0800

    Completed basic pseudocode logic for program.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // Create results array
+  // First get all of the elements in document.body
+  
+  // test first element with .className to check for 'targetClassName'
+    // if true, add to results 
+    
+      
+  // test for children greater than zero with childNodes.length > 0
+    //if true, loop through children
+      // for loop on childNodes.length calling getElementsByClassName
+    
+  // return results;
+  
+  
+  
+  
 };
